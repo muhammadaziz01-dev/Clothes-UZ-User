5e52d55f5cd1b81507d87eb94d9e4c44c94880cb
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-05-31 06:32:09 -1200

    categoty test 2

--- src/pages/category/index.tsx
+++ src/pages/category/index.tsx
@@ -1,6 +1,6 @@
 
 import { useParams } from "react-router-dom"
-import { useEffect , useState } from "react"
+import { useEffect ,  } from "react"
 
 import useCategoryStore from "@stor-category";
 import {Card} from "@ui"
